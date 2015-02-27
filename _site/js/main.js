cf97b6bcc854e11fbdf6850abc323aaf7861d7c6
asilvadesigns <asilvadesigns@gmail.com>
2015-02-27 14:03:36 -0500

    Update v74
    Test

--- _site/js/main.js
+++ _site/js/main.js
@@ -16,3 +16,29 @@ var navigation = responsiveNav(".nav--collapse", {
     open: function(){},               // Function: Open callback
     close: function(){}               // Function: Close callback
 });
+
+// Important note! If you're adding CSS3 transition to slides, fadeInLoadedSlide should be disabled to avoid fade-conflicts.
+jQuery(document).ready(function($) {
+    var si = $('#portfolio__gallery').royalSlider({
+        addActiveClass: true,
+        arrowsNav: false,
+        controlNavigation: 'none',
+        autoScaleSlider: true,
+        autoScaleSliderWidth: 960,
+        autoScaleSliderHeight: 340,
+        loop: true,
+        fadeinLoadedSlide: false,
+        globalCaption: true,
+        keyboardNavEnabled: true,
+        globalCaptionInside: false,
+
+        visibleNearby: {
+            enabled: true,
+            centerArea: 0.5,
+            center: true,
+            breakpoint: 650,
+            breakpointCenterArea: 0.64,
+            navigateByCenterClick: true
+        }
+    }).data('royalSlider');
+});

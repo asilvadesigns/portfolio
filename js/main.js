c5ba5326ee36bb42eeb4b584bbb9cf952e38fa5e
asilvadesigns <asilvadesigns@gmail.com>
2015-03-02 12:02:40 -0500

    Update v77
    Portfolio Slider Overhaul

--- js/main.js
+++ js/main.js
@@ -17,28 +17,10 @@ var navigation = responsiveNav(".nav--collapse", {
     close: function(){}               // Function: Close callback
 });
 
-// Important note! If you're adding CSS3 transition to slides, fadeInLoadedSlide should be disabled to avoid fade-conflicts.
-jQuery(document).ready(function($) {
-    var si = $('#portfolio__gallery').royalSlider({
-        addActiveClass: true,
-        arrowsNav: false,
-        controlNavigation: 'none',
-        autoScaleSlider: true,
-        autoScaleSliderWidth: 960,
-        autoScaleSliderHeight: 340,
-        loop: true,
-        fadeinLoadedSlide: false,
-        globalCaption: true,
-        keyboardNavEnabled: true,
-        globalCaptionInside: false,
-
-        visibleNearby: {
-            enabled: true,
-            centerArea: 0.5,
-            center: true,
-            breakpoint: 650,
-            breakpointCenterArea: 0.64,
-            navigateByCenterClick: true
-        }
-    }).data('royalSlider');
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    keyboardControl: true,
+    // Navigation arrows
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
 });
